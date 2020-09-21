88a8ed9ab1f5dea89eb1bbcbf6f2f5d0bcc45539
Jan Burse <janburse@fastmail.fm>
2020-09-21 14:30:56 +0200

    Launch Jekejeke Prolog 1.4.6

--- misc_others/vidal/quine.pl
+++ misc_others/vidal/quine.pl
@@ -1,7 +1,39 @@
-/*  File:    quineweb.pl
-    Created: Sep 16 2020
-    Purpose: help on https://swi-prolog.discourse.group/t/swi-prolog-php-html/2937
-*/
+/**
+ * Boole's Method from 1847
+ * https://stackoverflow.com/a/63603544/502187
+ * Variant that can check simultaneously tautology and antilogy.
+ *
+ * Verbose write statements and more
+ * firendly operators by Joseph Vidal-Rosset
+ *
+ * Warranty & Liability
+ * To the extent permitted by applicable law and unless explicitly
+ * otherwise agreed upon, XLOG Technologies GmbH makes no warranties
+ * regarding the provided information. XLOG Technologies GmbH assumes
+ * no liability that any problems might be solved with the information
+ * provided by XLOG Technologies GmbH.
+ *
+ * Rights & License
+ * All industrial property rights regarding the information - copyright
+ * and patent rights in particular - are the sole property of XLOG
+ * Technologies GmbH. If the company was not the originator of some
+ * excerpts, XLOG Technologies GmbH has at least obtained the right to
+ * reproduce, change and translate the information.
+ *
+ * Reproduction is restricted to the whole unaltered document. Reproduction
+ * of the information is only allowed for non-commercial uses. Selling,
+ * giving away or letting of the execution of the library is prohibited.
+ * The library can be distributed as part of your applications and libraries
+ * for execution provided this comment remains unchanged.
+ *
+ * Restrictions
+ * Only to be distributed with programs that add significant and primary
+ * functionality to the library. Not to be distributed with additional
+ * software intended to replace any components of the library.
+ *
+ * Trademarks
+ * Jekejeke is a registered trademark of XLOG Technologies GmbH.
+ */
 
 :- op(700, xfy, <=>).
 :- op(600, xfy, =>).
@@ -9,6 +41,13 @@
 :- op(400, xfy, &).
 :- op(300, fy, ~).
 
+/**
+ * eval(A, R):
+ * The predicate succeeds in R with a partial evaluated
+ * Boolean formula. The predicate starts with the leaves
+ * and calls simp after forming new nodes.
+ */
+% eval(+Formula, -Formula)
 eval(A, A) :- var(A), !.
 eval(A => B, R) :- !, eval(A, X), eval(B, Y), simp(X => Y, R).
 eval(A <=> B, R) :- !, eval(A, X), eval(B, Y), simp(X <=> Y, R).
@@ -17,6 +56,13 @@ eval(A & B, R) :- !, eval(A, X), eval(B, Y), simp(X & Y, R).
 eval(~A, R) :- !, eval(A, X), simp(~X, R).
 eval(A, A).
 
+/**
+ * simp(A, B):
+ * The predicate succeeds in B with a simpler formula than A,
+ * or if an immediately simpler formula is not possible with
+ * formula B itself.
+ */
+% simp(+Formula, -Formula)
 simp(A, A) :- var(A), !.
 simp(A => _, 1) :- A == 0, !.
 simp(A => B, X) :- B == 0, !, simp(~A, X).
